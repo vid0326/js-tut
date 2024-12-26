a69b2e40c878a43c607bcefe454ed808be27bab7
vid0326 <vidhutraushan9@gmail.com>
2024-12-26 22:16:49 +0530

    learned about the some dataTypes of the js

---
+++ dataType_basics.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all the js code as newer version 
+
+// alert(3+3 ) we can not directlty use alert here .. we will explore later
+
+
+let name = "vidhut" // string
+
+let age = 21//int 
+let islogg = false
+
+// number => 2 to power 53
+
+// string =>""
+// boolean => true or false
+// null => nothing , standalone value
+
+// undefined =>value is not assigned
+//symbol => unique
+
+//object
+
+// console.log(typeof age)
+// console.log(typeof null) o/p=>object
+// console.log(typeof undefined) o/p=>undefined
